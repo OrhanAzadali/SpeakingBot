c93652f8d39c9501571e5e81403cb9b74cab42d9
OrhanAzadali <orhanazadali@gmail.com>
2026-08-29 17:08:41 +0400

    asteriskupd

--- bot/index.js
+++ bot/index.js
@@ -198,7 +198,7 @@ bot.on("message:voice" || "message:text", async (ctx) => {
   const userId = ctx.from.id;
   const user = await getUser(userId);
 
-  if (!user || user.state !== "chatting") {
+  if (!user) {
     await ctx.reply("Please /start first to choose your language.");
     return;
   }
