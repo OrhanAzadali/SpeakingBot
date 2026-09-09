4fa89f783682a5a631afde5ee93d3b3f91c13c9b
OrhanAzadali <orhanazadali@gmail.com>
2026-09-09 19:52:56 +0400

    aithemes

--- src/App.jsx
+++ src/App.jsx
@@ -398,7 +398,6 @@ function MainApp() {
         activeTheme={activeTheme}
         onOpenTestModal={() => setShowTestModal(true)}
         onOpenLanguageModal={() => setShowLangModal(true)}
-        onUiLanguageChange={setUiLanguage}
       />
       {
         /* Main Workspace Body */
